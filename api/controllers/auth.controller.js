f5c38b01da64c3738554296e9ebd13695dc4532f
dev lohani <devlohani14251425@gmail.com>
2024-11-19 23:37:34 +0530

    complete signin page fun

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
-import jwt from 'jsonwebtoken';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -29,14 +29,14 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(401, "Invalid password"));
     }
-    const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET)
-    const {password :pass,...rest} = validUser._doc;
-    res.cookie('token',token,{httpOnly:true});
-    res.cookie('access_token',token,{httpOnly:true}).
-    status(200).json(rest);
-   
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("token", token, { httpOnly: true });
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (err) {
     next(err);
   }
 };
- 
